4b0664793f7e9c205496ed273539a176bd016e3a
musha09cz@gmail.com <musha09cz@gmail.com>
2026-05-19 00:40:52 +0200

    feat: add footer

---
+++ src/components/layout/footer/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="mt-[7.47625rem]">
+        <picture>
+            <source srcset="@img/footer/footer.avif" type="image/avif" />
+            <img src="@img/footer/footer.webp" alt="" />
+        </picture>
+
+        <div class="bg-base-black -mt-px min-h-167.5"></div>
+    </section>
+</template>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import AppFooter from '@/components/layout/footer/AppFooter.vue';
 import AppHeader from '@/components/layout/header/AppHeader.vue';
 </script>
 
 <template>
     <div>
         <AppHeader />
+        <AppFooter />
     </div>
 </template>
